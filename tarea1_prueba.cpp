376185c2828c365e927b42d92fbd38e9a7a22c49
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-02 13:33:22 -0400

    Signed-off-by: Ricardo Tapia <RickTC@localhost.localdomain>

--- tarea1_prueba.cpp
+++ tarea1_prueba.cpp
@@ -7,6 +7,10 @@ int main()
  {
 	
 	cout<<"Holo mundo"<<endl<<"Este es mi primer trabajo en Programacion 3 :)"<<endl;
+		cout<<"Suma de 2 numeros";
+			int l=2,m=3;
+				cout<<endl<<"suma de 2+3= "<<l+m<<endl<<"Fin de mi tarea";
 
 		return EXIT_SUCCESS;
+
  }
