8c213edbdb802bf1d9a279128079e90f77c0ec41
Ricardo Tapia <RickTC@localhost.localdomain>
2013-09-02 13:12:55 -0400

    Signed-off-by: Ricardo Tapia <RickTC@localhost.localdomain>

---
+++ tarea de prueba.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+
+main()
+	
+ {
+	cout<<"Hola Mundo"<<endl<<"Este es mi primer trabajo de programacion 3 ;)"<<endl;
+ 																											
+ 
+
+ 
+ 
+ }
